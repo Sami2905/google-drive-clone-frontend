48e0b3098bf544a2322861f62ed4ee89dc5e7055
Samiullah <skmdsamiullah40@gmail.com>
2025-08-24 12:50:52 +0530

    Simplify next.config.js to minimal configuration to fix Html component build error

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', '127.0.0.1'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.amazonaws.com',
-      },
-    ],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
