2e44cb54fdfa0ef9849e7d1a51bb38fcdfba1060
Samiullah <skmdsamiullah40@gmail.com>
2025-08-25 10:49:59 +0530

    chore: enable devDeps on Netlify; add @tailwindcss/postcss; fix @ alias; ensure modules exist

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   generateBuildId: async () => {
     return 'build-' + Date.now()
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
